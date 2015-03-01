8acff43caada309394510cc241fdf585ada50613
Jorge Hernán <jorge.hernan.esparza@gmail.com>
2015-03-01 10:49:56 +0100

    Test Gutter and AllOnes

--- src/Kata/Game.php
+++ src/Kata/Game.php
@@ -7,5 +7,13 @@ namespace Kata;
  */
 class Game
 {
-    
+    private $score = 0;
+
+    public function roll($pins){
+        $this->score += $pins;
+    }
+
+    public function score(){
+        return $this->score;
+    }
 }
--- src/Kata/Tests/BowlingGameTest.php
+++ src/Kata/Tests/BowlingGameTest.php
@@ -5,7 +5,29 @@ use Kata\Game;
 
 class BowlingGameTest extends \PHPUnit_Framework_TestCase
 {
+    private $game;
+
+    public function setUp(){
+        $this->game = new Game();
+    }
+
     public function testGutterGame(){
-        $game = new Game();
+        $n = 20;
+        $pins = 0;
+        $this->rollMany($n, $pins);
+        $this->assertEquals(0, $this->game->score());
+    }
+
+    public function testAllOnes(){
+        $n = 20;
+        $pins = 1;
+        $this->rollMany($n, $pins);
+        $this->assertEquals(20, $this->game->score());
+    }
+
+    private function rollMany($n, $pins){
+        for($i = 0; $i < $n; $i++){
+            $this->game->roll($pins);
+        }
     }
 }
